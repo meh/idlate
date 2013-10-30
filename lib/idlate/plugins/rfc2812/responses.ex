354a35c9912e9d07afe596a8160ae3c0f9e41bea
jibi <jibi@paranoici.org>
2013-10-30 15:41:26 +0100

    rfc2812: add bunch of numeric responses

--- lib/idlate/plugins/rfc2812/responses.ex
+++ lib/idlate/plugins/rfc2812/responses.ex
@@ -5,3 +5,46 @@ defnumeric Response.Welcome, 1, [:server, :mask] do
     ":Welcome to the #{server} #{mask}"
   end
 end
+
+defnumeric Response.HostedBy, 2, [:bind, :ip, :port, :version] do
+  def to_string(__MODULE__[bind: bind, ip: ip, port: port, version: version]) do
+    ":Your host is #{bind}[#{ip}/#{port}], running version idlate-#{version}"
+  end
+end
+
+defnumeric Response.ServCreatedOn, 3, [:created_on] do
+  def to_string(__MODULE__[created_on: created_on]) do
+	":This server was created #{created_on}"
+  end
+end
+
+defnumeric Response.ServInfo, 4, [:host, :version, :client, :channel] do
+  def to_string(__MODULE__[host: host, version: version, client: client, channel: channel]) do
+      "#{host} idlate-#{version} #{client} #{channel}"
+  end
+end
+
+defnumeric Resopnse.IsSupport, 5, [:value] do
+  def to_string(__MODULE__[value: value]) do
+	"#{value} :are supported by this server"
+  end
+end
+
+defnumeric Response.ChanCreatedOn, 329, [:name, :created_on] do
+  def to_string(__MODULE__[name: name, created_on: created_on]) do
+    "#{name} #{created_on}"
+  end
+end
+
+defnumeric Response.TopicSetOn, 333, [:channel_name, :set_by, :set_on] do
+  def to_string(__MODULE__[channel_name: channel_name, set_by: set_by, set_on: set_on]) do
+	"#{channel_name} #{set_by} #{set_on}"
+  end
+end
+
+defnumeric Response.UsingSSL, 671, [:nick] do
+  def to_string(__MODULE__[nick: nick]) do
+	"#{nick} :is using a Secure Connection"
+  end
+end
+
