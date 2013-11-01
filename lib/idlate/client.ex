728010f7d892c3503ff890331fa3ac9a14ea1196
meh <meh@schizofreni.co>
2013-11-01 19:44:36 +0100

    client: save IP and hostname when the client connects

--- lib/idlate/client.ex
+++ lib/idlate/client.ex
@@ -7,44 +7,55 @@ defmodule Idlate.Client do
 
   use GenServer.Behaviour
 
+  defrecord State, connection: nil, ip: nil, port: nil, host: nil
+
   def init([connection]) do
-    { :ok, connection }
+    { ip, port } = connection |> Socket.remote!
+    host         = case Socket.Host.by_address(ip) do
+      { :ok, Socket.Host[name: name] } ->
+        name
+
+      { :error, _ } ->
+        Socket.Address.format(ip)
+    end
+
+    { :ok, State[connection: connection, ip: ip, port: port, host: host] }
   end
 
-  def handle_info({ Reagent, :ack }, connection) do
+  def handle_info({ Reagent, :ack }, State[connection: connection] = _state) do
     connection |> Socket.packet! :line
 
     :gen_server.cast Idlate, { Process.self, :connected }
 
-    { :noreply, connection }
+    { :noreply, _state }
   end
 
-  def handle_info({ :tcp, _, line }, _connection) do
+  def handle_info({ :tcp, _, line }, _state) do
     :gen_server.cast Idlate, { Process.self, :sent, String.rstrip(line) }
 
-    { :noreply, _connection }
+    { :noreply, _state }
   end
 
-  def handle_info({ :tcp_closed, reason }, _connection) do
+  def handle_info({ :tcp_closed, reason }, _state) do
     :gen_server.cast Idlate, { Process.self, :disconnected, reason }
 
-    { :noreply, _connection }
+    { :noreply, _state }
   end
 
-  def handle_cast(:shutdown, _connection) do
-    { :stop, :normal, _connection }
+  def handle_cast(:shutdown, _state) do
+    { :stop, :normal, _state }
   end
 
-  def handle_cast(:handled, connection) do
+  def handle_cast(:handled, State[connection: connection] = _state) do
     connection |> Socket.active! :once
 
-    { :noreply, connection }
+    { :noreply, _state }
   end
 
-  def handle_cast({ :send, data }, connection) do
+  def handle_cast({ :send, data }, State[connection: connection] = _state) do
     Enum.each List.wrap(data), &Socket.Stream.send!(connection, [&1, "\r\n"])
 
-    { :noreply, connection }
+    { :noreply, _state }
   end
 
   def send(pid, data) do
@@ -53,5 +64,21 @@ defmodule Idlate.Client do
 
   def handled(pid) do
     :gen_server.cast(pid, :handled)
+  end
+
+  def handle_call(:ip, _from, State[ip: ip] = _state) do
+    { :reply, ip, _state }
+  end
+
+  def handle_call(:host, _from, State[host: host] = _state) do
+    { :reply, host, _state }
+  end
+
+  def ip(pid) do
+    :gen_server.call(pid, :ip)
+  end
+
+  def host(pid) do
+    :gen_server.call(pid, :host)
   end
 end
