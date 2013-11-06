1a9cde802190b43e12d2e2116007d2b548911967
meh <meh@schizofreni.co>
2013-11-06 20:51:42 +0100

    rfc281x: add some events

--- lib/idlate/plugins/rfc281x/events.ex
+++ lib/idlate/plugins/rfc281x/events.ex
@@ -3,4 +3,10 @@ use Idlate.RFC281X.DSL
 defevent Password, content: nil
 defevent Nick, name: nil
 defevent User, name: nil, real_name: nil, modes: nil
+
+defevent Ping, cookie: nil
+defevent Pong, cookie: nil
+
+defevent Join, channel: nil, password: nil
+defevent Part, channel: nil, reason: nil
 defevent Message, from: nil, to: nil, content: nil
