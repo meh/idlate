7a268d31b0ee08032e8968daea16361796fd126d
meh <meh@schizofreni.co>
2016-05-22 00:06:05 +0200

    *: add LICENSE

--- lib/idlate/supervisor.ex
+++ lib/idlate/supervisor.ex
@@ -1,3 +1,20 @@
+# Copyleft (ɔ) meh. - http://meh.schizofreni.co
+#
+# This file is part of idlate - https://github.com/meh/idlate
+#
+# idlate is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# idlate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with idlate. If not, see <http://www.gnu.org/licenses/>.
+
 defmodule Idlate.Supervisor do
   import Supervisor.Spec
 
--- mix.exs
+++ mix.exs
@@ -4,19 +4,26 @@ defmodule Idlate.Mixfile do
   def project do
     [ app: :idlate,
       version: "0.0.1",
-      deps: deps ]
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      package: package,
+      description: "Plugin based IRC server." ]
   end
 
-  # Configuration for the OTP application
   def application do
-    [ applications: [:socket],
-      mod: { Idlate, [config: "config.exs"] } ]
+    [ applications: [:socket, :logger],
+      mod: { Idlate, [config: "config/server.exs"] } ]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ { :reagent, "~> 0.1" },
+    [ { :reagent, github: "meh/reagent" },
       { :datastructures, "~> 0.2" } ]
+  end
+
+  defp package do
+    [ maintainers: ["meh"],
+      licenses: ["AGPLv3"],
+      links: %{"GitHub" => "https://github.com/meh/idlate"} ]
   end
 end
